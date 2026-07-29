149eb1ce4849faa90c58c97c4536fc84b3f89d81
Petr Fedin <81327591+PetrFedin@users.noreply.github.com>
2026-07-29 13:49:25 +0300

    Load notification retry model in Alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from backend import model_constraints, models  # noqa: F401
+from backend import model_constraints, models, notification_models  # noqa: F401
 from backend.database import Base
 
 config = context.config
