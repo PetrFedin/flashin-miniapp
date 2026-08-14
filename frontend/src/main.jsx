a069da75ade14ba4d17b9b6eba242e3f568b6d60
Petr Fedin <81327591+PetrFedin@users.noreply.github.com>
2026-08-15 02:24:27 +0300

    Mount demand request experience in Mini App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import CatalogExperience from './CatalogExperience';
+import DemandExperience from './DemandExperience';
 import SharedProductLanding from './SharedProductLanding';
 import './styles.css';
 import './catalog-plus.css';
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
     <>
       <App />
       <CatalogExperience />
+      <DemandExperience />
       <SharedProductLanding />
     </>
   </React.StrictMode>
