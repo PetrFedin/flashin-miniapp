0132309ce899b621362bb5f69f41b65436e52921
Petr Fedin <81327591+PetrFedin@users.noreply.github.com>
2026-08-14 12:25:25 +0300

    Mount Catalog+ in Mini App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import CatalogExperience from './CatalogExperience';
 import './styles.css';
+import './catalog-plus.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <CatalogExperience />
+    </>
   </React.StrictMode>
 );
